e31d3b0d368777b8c5f97ebbd3ad7322905b6a07
Kavita <kavita.c.argekar@gmail.com>
2020-07-27 17:31:47 +0530

    commiting

---
+++ BankingApplication/src/com/arwizon/banking/main/BankingApplication.java
@@ -0,0 +1,23 @@
+package com.arwizon.banking.main;
+
+import com.arwizon.banking.model.Customer;
+
+public class BankingApplication {
+	
+	
+	public static void main(String args[]) {
+		
+		
+		
+		Customer[] cst= new Customer[3];
+		
+		
+			
+		
+		System.out.println(cst);
+		
+		
+		
+	}
+
+}
---
+++ BankingApplication/src/com/arwizon/banking/model/Customer.java
@@ -0,0 +1,66 @@
+package com.arwizon.banking.model;
+
+public class Customer  {
+	
+	private String name;
+	private int customerId;
+	private String address;
+	private String mobileNo;
+	private String accounType;
+	
+	public Customer() {
+		
+	}
+	
+	public Customer(String name) {
+		this();
+		this.name=name;		
+	}
+	
+	public String getName() {		
+		return name;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getAccounType() {
+		return accounType;
+	}
+	public void setAccounType(String accounType) {
+		this.accounType = accounType;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", customerId=" + customerId + ", address=" + address + ", mobileNo="
+				+ mobileNo + ", accounType=" + accounType + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
